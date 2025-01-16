b6ca27c1b28770e32da61a02d605fcb088120fc0
Gabriel Q. <118689676+gabiqtn@users.noreply.github.com>
2025-01-16 01:34:48 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/login/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller.login;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping("/login")
+//public class LoginController {
+//    @PostMapping
+//    public String login() {
+//        return "Login successful!";
+//    }
+//}
---
+++ src/main/java/com/example/demo/controller/login/RegisterController.java
@@ -0,0 +1,4 @@
+package com.example.demo.controller.login;
+
+public class RegisterController {
+}
